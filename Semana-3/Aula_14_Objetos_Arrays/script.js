848a2452ef881939dbb5f70e5cd98b26c54188f8
Davi Duarte <daviduarte973@gmail.com>
2019-09-24 16:28:36 -0300

    Almost wrapping up

--- Semana-3/Aula_14_Objetos_Arrays/script.js
+++ Semana-3/Aula_14_Objetos_Arrays/script.js
@@ -12,28 +12,37 @@ function salvaItem(chave, item) {
     }
 }
 
-// function apagaItems() {
-//     document.getElementById("in-name").value = "";
-//     document.getElementById("in-idade").value = "";
-//     document.getElementById("in-email").value = "";
-// }
-
-
+let nome = [];
+let idade = [];
+let e_mail = [];
 
 function tesTe() {
-    const nome = document.getElementById("in-name").value;
-    const idade = document.getElementById("in-idade").value;
-    const e_mail = document.getElementById("in-email").value
+    nome.push(document.getElementById("in-name").value);
+    idade.push(document.getElementById("in-idade").value);
+    e_mail.push(document.getElementById("in-email").value);
 
     salvaItem("nome", nome);
-    salvaItem("idade", 28);
-    salvaItem("email", "davi.duarte@gmail.com");
+    salvaItem("idade", idade);
+    salvaItem("email", e_mail);
+
+    nome = buscaItem("nome");
+    idade = buscaItem("idade");
+    e_mail = buscaItem("email");
 
     const userName = {
-        name: [nome],
-        age: [idade],
-        email: [e_mail]
+        name: nome,
+        age: idade,
+        email: e_mail
     }
 
     console.log('objeto userName: ', userName);
+
+
+    console.log(userName.name[0]);
+
+    document.getElementById("in-name").value = "";
+    document.getElementById("in-idade").value = "";
+    document.getElementById("in-email").value = "";
 }
+
+
